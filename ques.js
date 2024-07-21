2a7ae869d676ed6fac21590440c0894eb62b9aaa
Aritra Parui <122464998+TechnoAri03@users.noreply.github.com>
2024-07-21 12:28:41 +0530

    Add files via upload

---
+++ ques.js
@@ -0,0 +1,39 @@
+export const data = [
+
+    {
+        question: "Which of the following is the brain of the computer?",
+        opton1: "Memory",
+        opton2: "CPU",
+        opton3: "ALU",
+        opton4: "Control Unit",
+        ans: 2
+    },
+    {
+        question: "What does RAM stand for?",
+        opton1: "Read Access Memory",
+        opton2: "Random Access Memory",
+        opton3: "Read Any Memory",
+        opton4: "Read Access Module",
+        ans: 2
+    },
+    {
+        question: "Which type of software controls the internal operations in the computer?",
+        opton1: "System Software",
+        opton2: "Application Software",
+        opton3: "Utility Software",
+        opton4: "Malicious Software",
+        ans: 1
+    },
+    {
+        question: "Which of the following is an example of an input device?",
+        opton1: "Monitor",
+        opton2: "Printer",
+        opton3: "Keyboard",
+        opton4: "Speaker",
+        ans: 3
+    },
+
+
+
+
+]
